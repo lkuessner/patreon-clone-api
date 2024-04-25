a2ad99a963c98acdee07a1ab08cc1ad6b167ceef
PredaDavid <davidpreda.dev@gmail.com>
2024-04-25 22:07:02 +0300

    Fix: Mistook APIView with ViewSet
    - I didn't understand correctly how to use an APIView and a ViewSet. I thought that the two things are related and both needed to be implemented at the same time.
    - Now I understand that the APIView is a single point url, that is meant to be mapped inside urls.py while ViewSet include multiple crud operations and are meant to be use with routers.
    - Till this point the api worked because the APIView's urls were added first (before the router's urls), so when you went to localhost/api/v1/register you recieve the APIView not the router ViewSet. At the same time the links were present in api root because I included them in the router.
    - I made the changes that I believe will fix my confustion, for now I just commented the cod that needs to be deleted before I receive a green light for my changes.

--- src/users/api/v1/urls.py
+++ src/users/api/v1/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from .viewsets import RegisterView,LoginView,UserView,LogoutView
+# from .viewsets import RegisterView,LoginView,UserView,LogoutView
 
 urlpatterns = [
-    path('register/', RegisterView.as_view()),
-    path('login/', LoginView.as_view()),
-    path('user/', UserView.as_view()),
-    path('logout/', LogoutView.as_view())
+    # path('register/', RegisterView.as_view()),
+    # path('login/', LoginView.as_view()),
+    # path('user/', UserView.as_view()),
+    # path('logout/', LogoutView.as_view())
 ]
